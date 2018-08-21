e5e5687b2b2126c557e224352ad22870061adbb1
Pedro Amaral <pamaral@codacy.com>
2018-08-21 21:22:46 +0100

    add tests

---
+++ src/test/resources/codacy/duplication/php/FileWithDuplication.php
@@ -0,0 +1,45 @@
+<?php
+class BinaryTree
+{
+
+    public function insertooo($item) {
+        $node = new BinaryNode($item);
+        if ($this->isEmpty()) {
+            // special case if tree is empty
+            $this->root = $node;
+        }
+        else {
+            // insert the node somewhere in the tree starting at the root
+            $this->insertNode($node, $this->root);
+        }
+        $node = new BinaryNode($item);
+        if ($this->isEmpty()) {
+            // special case if tree is empty
+            $this->root = $node;
+        }
+        else {
+            // insert the node somewhere in the tree starting at the root
+            $this->insertNode($node, $this->root);
+        }
+    }
+
+    protected function insertNode($node, &$subtree) {
+        if ($subtree === null) {
+            // insert node here if subtree is empty
+            $subtree = $node;
+        }
+        else {
+            if ($node->value > $subtree->value) {
+                // keep trying to insert right
+                $this->insertNode($node, $subtree->right);
+            }
+            else if ($node->value < $subtree->value) {
+                // keep trying to insert left
+                $this->insertNode($node, $subtree->left);
+            }
+            else {
+                // reject duplicates
+            }
+        }
+    }
+}
---
+++ src/test/scala/codacy/duplication/phpcpd/PHPCPDSpec.scala
@@ -0,0 +1,101 @@
+package codacy.duplication.phpcpd
+
+import codacy.docker.api.Source
+import codacy.docker.api.duplication.{DuplicationClone, DuplicationCloneFile}
+import com.codacy.api.dtos.Languages
+import org.specs2.mutable.Specification
+
+import scala.util.{Failure, Success, Try}
+
+class PHPCPDSpec extends Specification {
+
+  private val expectedCloneLines =
+    """    public function insert($item) {
+      |        $node = new BinaryNode($item);
+      |        if ($this->isEmpty()) {
+      |            // special case if tree is empty
+      |            $this->root = $node;
+      |        }
+      |        else {
+      |            // insert the node somewhere in the tree starting at the root
+      |            $this->insertNode($node, $this->root);
+      |        }
+      |        $node = new BinaryNode($item);
+      |        if ($this->isEmpty()) {
+      |            // special case if tree is empty
+      |            $this->root = $node;
+      |        }
+      |        else {
+      |            // insert the node somewhere in the tree starting at the root
+      |            $this->insertNode($node, $this->root);
+      |        }
+      |    }
+      |
+      |
+      |    protected function insertNode($node, &$subtree) {
+      |        if ($subtree === null) {
+      |            // insert node here if subtree is empty
+      |            $subtree = $node;
+      |        }
+      |        else {
+      |            if ($node->value > $subtree->value) {
+      |                // keep trying to insert right
+      |                $this->insertNode($node, $subtree->right);
+      |            }
+      |            else if ($node->value < $subtree->value) {
+      |                // keep trying to insert left
+      |                $this->insertNode($node, $subtree->left);
+      |            }
+      |""".stripMargin
+  private val expectedDuplication = List(
+    DuplicationClone(
+      cloneLines = expectedCloneLines,
+      nrTokens = 83,
+      nrLines = 36,
+      files = List(
+        DuplicationCloneFile("codacy/duplication/php/FileWithDuplication1.php",
+                             4,
+                             40),
+        DuplicationCloneFile("codacy/duplication/php/FileWithDuplication.php",
+                             5,
+                             41)
+      )
+    ))
+
+  private val targetDir = "src/test/resources/"
+
+  "PHPCPD" should {
+
+    "get duplication results" in {
+
+      val duplicationResults: Try[List[DuplicationClone]] = PHPCPD(
+        path = Source.Directory(targetDir),
+        language = Some(Languages.PHP),
+        options = Map.empty
+      )
+
+      duplicationResults must beLike {
+        case Success(duplication) =>
+          duplication must beEqualTo(expectedDuplication)
+      }
+
+    }
+
+    "fail" in {
+      "wrong language" in {
+        val duplicationResults: Try[List[DuplicationClone]] = PHPCPD(
+          path = Source.Directory(targetDir),
+          language = Some(Languages.Scala),
+          options = Map.empty
+        )
+
+        duplicationResults must beLike {
+          case Failure(e) =>
+            e.getMessage must beEqualTo(
+              s"PHPCPD only supports PHP. Provided language: ${Languages.Scala.name}")
+        }
+      }
+    }
+
+  }
+}
