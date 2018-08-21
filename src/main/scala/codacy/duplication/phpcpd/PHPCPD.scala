f71160ba965b3e2d672ebc71a612ef70fb7875f3
Pedro Amaral <pamaral@codacy.com>
2018-08-21 21:21:58 +0100

    update tool

--- src/main/scala/codacy/duplication/phpcpd/PHPCPD.scala
+++ src/main/scala/codacy/duplication/phpcpd/PHPCPD.scala
@@ -1,50 +1,66 @@
 package codacy.duplication.phpcpd
 
 import java.io.File
-import java.nio.file.{Files, Path, Paths}
+import java.nio.file.Paths
 
-import codacy.dockerApi.api.{DuplicationClone, DuplicationCloneFile, DuplicationConfiguration}
-import codacy.dockerApi.traits.IDuplicationImpl
-import codacy.dockerApi.utils.CommandRunner
+import codacy.docker.api.{DuplicationConfiguration, Source}
+import codacy.docker.api.duplication.{
+  DuplicationClone,
+  DuplicationCloneFile,
+  DuplicationTool
+}
+import codacy.docker.api.utils.{CommandResult, CommandRunner}
+import com.codacy.api.dtos.{Language, Languages}
 
-import scala.util.{Failure, Properties, Success, Try}
+import scala.util.{Failure, Properties, Try}
 import scala.xml.{Elem, XML}
 
-object PHPCPD extends IDuplicationImpl {
+object PHPCPD extends DuplicationTool {
 
-  override def apply(rootPath: Path, config: DuplicationConfiguration): Try[List[DuplicationClone]] = {
-    val rootDirectory = rootPath.toFile
-    val temporaryFile = Files.createTempFile("codacy-", ".xml").toFile
-    CommandRunner.exec(getCommand(rootDirectory, temporaryFile), Some(rootDirectory)) match {
-      case Right(resultFromTool) =>
-        parseToolResult(rootDirectory, temporaryFile) match {
-          case s@Success(_) => s
-          case Failure(e) =>
-            val msg =
-              s"""
-                 |PHPCPD exited with code ${resultFromTool.exitCode}
-                 |message: ${e.getMessage}
-                 |stdout: ${resultFromTool.stdout.mkString(Properties.lineSeparator)}
-                 |stderr: ${resultFromTool.stderr.mkString(Properties.lineSeparator)}
-                 |StackTrace: ${e.getStackTrace.mkString(System.lineSeparator)}
-                """.stripMargin
-            Failure(new Exception(msg))
+  def apply(path: Source.Directory,
+            language: Option[Language],
+            options: Map[DuplicationConfiguration.Key,
+                         DuplicationConfiguration.Value])
+    : Try[List[DuplicationClone]] = {
+    language match {
+      case Some(lang) if lang != Languages.PHP =>
+        Failure(
+          new Exception(
+            s"PHPCPD only supports PHP. Provided language: ${lang.name}"))
+      case _ =>
+        val rootDirectory = better.files.File(path.path).toJava
+        withOutputFile { outputFile: File =>
+          runTool(rootDirectory, outputFile).flatMap { resultFromTool =>
+            parseToolResult(rootDirectory, outputFile).recoverWith {
+              case e => handleFailure(resultFromTool, e)
+            }
+          }
         }
-
-      case Left(e) =>
-        Failure(e)
     }
   }
 
-  private def parseToolResult(rootDirectory: File, temporaryFile: File): Try[List[DuplicationClone]] = {
+  private def runTool(rootDirectory: File,
+                      outputFile: File): Try[CommandResult] = {
+    val command = getCommand(rootDirectory, outputFile)
+    CommandRunner.exec(command, Option(rootDirectory)).toTry
+  }
+
+  private def parseToolResult(
+      rootDirectory: File,
+      temporaryFile: File): Try[List[DuplicationClone]] = {
     Try(XML.loadFile(temporaryFile)).flatMap(parseXml(rootDirectory, _))
   }
 
-  private def getCommand(rootDirectory: File, outputFile: File): List[String] = {
-    List("php", "-d", "memory_limit=-1", "/bin/phpcpd", "--log-pmd", outputFile.getCanonicalPath, rootDirectory.getCanonicalPath)
+  private def getCommand(rootDirectory: File,
+                         outputFile: File): List[String] = {
+    List("phpcpd",
+         "--log-pmd",
+         outputFile.getCanonicalPath,
+         rootDirectory.getCanonicalPath)
   }
 
-  private def parseXml(rootDirectory: File, elem: Elem): Try[List[DuplicationClone]] = {
+  private def parseXml(rootDirectory: File,
+                       elem: Elem): Try[List[DuplicationClone]] = {
     Try {
       (elem \ "duplication").map { duplication =>
         val tokens = (duplication \ "@tokens").text.toInt
@@ -52,7 +68,8 @@ object PHPCPD extends IDuplicationImpl {
 
         val files = (duplication \ "file").map { file =>
           val filePath = (file \ "@path").text
-          val relativePath = rootDirectory.toPath.relativize(Paths.get(filePath)).toString
+          val relativePath =
+            rootDirectory.toPath.relativize(Paths.get(filePath)).toString
           val line = (file \ "@line").text.toInt
 
           DuplicationCloneFile(relativePath, line, line + lines)
@@ -60,8 +77,28 @@ object PHPCPD extends IDuplicationImpl {
 
         val code = (duplication \ "codefragment").text
         DuplicationClone(code, tokens, lines, files)
-      }.toList
+      }(collection.breakOut)
     }
+  }
+
+  private def withOutputFile[T](fn: File => T): T = {
+    (for {
+      file <- better.files.File.temporaryFile(suffix = ".xml")
+    } yield {
+      fn(file.toJava)
+    }).get()
+  }
+
+  private def handleFailure(resultFromTool: CommandResult, e: Throwable) = {
+    val msg =
+      s"""
+         |PHPCPD exited with code ${resultFromTool.exitCode}
+         |message: ${e.getMessage}
+         |stdout: ${resultFromTool.stdout.mkString(Properties.lineSeparator)}
+         |stderr: ${resultFromTool.stderr.mkString(Properties.lineSeparator)}
+         |StackTrace: ${e.getStackTrace.mkString(System.lineSeparator)}
+                """.stripMargin
+    Failure(new Exception(msg))
   }
 
 }
